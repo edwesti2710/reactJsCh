9e3ad7e3774c0c7711774e2bb361a94d91833277
Edward Espinoza <edwesti2710@gmail.com>
2022-10-23 20:41:33 -0500

    First commit

---
+++ public/components/header.jsx
@@ -0,0 +1,9 @@
+<header>
+    <h1>PCBuild</h1>
+    <ul>
+        <li><a href="www.google.com">Home</a></li>
+        <li><a href="www.google.com">Ofertas</a></li>
+        <li><a href="www.google.com">Nosotros</a></li>
+        <li><a href="www.google.com">Iniciar Sesión</a></li>
+    </ul>
+</header>
